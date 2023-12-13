478c06f1ad230b88a9c7155e0417a6d05607ee23
theGreatCarrer <alejandroyroky@gmail.com>
2023-12-13 20:04:15 +0100

    Adios js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const secret2 = document.getElementById("secret2");
 const secret3 = document.getElementById("secret3");
 const login = document.getElementById("login");
 
-/*
+
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = user.value;
@@ -24,4 +24,4 @@ loginButton.addEventListener("click", (e) => {
         login.classList.add("hide")
     }
 }
-)*/
+)
